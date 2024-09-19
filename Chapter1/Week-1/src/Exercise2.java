f8db3f940fe32baf6360370f14796ee6d4297a48
wyynb666 <143530983+wyynb666@users.noreply.github.com>
2024-09-19 17:51:14 +0800

    Add files via upload

---
+++ Chapter1/Week-1/src/Exercise2.java
@@ -0,0 +1,7 @@
+public class Exercise2 {
+    public static void main(String[] args) {
+        double result;
+        result = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5);
+        System.out.println(result);
+    }
+}
